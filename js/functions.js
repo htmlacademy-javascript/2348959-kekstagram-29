96a0d5b7d44e4146c88c5ac4af9f6d8282522560
dklobov <d.k.lobov@gmail.com>
2023-06-19 16:43:03 +0300

    создан файл функций.js и добавлен в index.html

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+//функция для проверки длины строки
+
+const checkStringLength = (string, length) => string.length <= length;
+checkStringLength ('строка', 20);
+
+
+
+//функция для проверки является ли строка палиндромом
+
+function isPalindrom(rndmString) {
+  const string = rndmString.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++ ) {
+    if (string.at(i) !== string.at(-i -1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(isPalindrom('камаз'));
+// console.log(isPalindrom('tenet'));
+// console.log(isPalindrom('Аргентина манит негра'));
